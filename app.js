da7c20819332b7987835c514fb3f85fe8f57f912
Patrick Pelletier <pp.pelletier@gmail.com>
2017-04-03 23:00:38 -0400

    add evenements

--- app.js
+++ app.js
@@ -4,8 +4,32 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
 
+evenements = [
+    {
+        nom: "poisson d'avril",
+        date: "2017/04/01",
+        categorie: "fete",
+        descriptions: "on fait des farces",
+        lien: "http://www.poisson.com",
+    },
+    {
+        nom: "noel",
+        date: "2017/12/25",
+        categorie: "fete",
+        descriptions: "j'mange dla tourtiere",
+        lien: "http://www.noel.com"
+    },
+]
+
 app.get('/', function(req, res){
-    res.send("hello")
+    text = "Liste d'evenements:<br>"
+    text += "<table>"
+    for (i = 0; i < evenements.length; i++) {
+        text += "<tr>" + "<td><b>" + evenements[i].date + "</b></td>"
+        text += "<td><a href=\"" + evenements[i].lien + "\">" + evenements[i].nom + "</a></tr></tr>";
+    }
+    text += "</table>"
+    res.send(text)
 })
 
 app.listen('8081')
